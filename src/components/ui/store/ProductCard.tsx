ae7ae467e2263e3765c3e7c6e6d457b7e45369ee
alex-rublevsky <alexander.rublevskii@gmail.com>
2025-05-21 11:45:45 -0400

    feat: Refactor product routes and implement store product page with API integration

--- src/components/ui/store/ProductCard.tsx
+++ src/components/ui/store/ProductCard.tsx
@@ -173,197 +173,62 @@ function ProductCard({ product }: { product: ProductWithVariations }) {
   }, []);
 
   return (
-    // <Link
-    //   to="/store/$itemId"
-    //   params={{
-    //     itemId: product.slug,
-    //   }}
-    //   className="block h-full relative"
-    //   onMouseEnter={handleMouseEnter}
-    //   onMouseLeave={handleMouseLeave}
-    //   onClick={handleCardClick}
-    // >
-    <div
-      className="w-full product-card overflow-hidden rounded-lg group"
-      id={styles.productCard}
+    <Link
+      to="/store/$productId"
+      params={{
+        productId: product.slug,
+      }}
+      className="block h-full relative"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleCardClick}
     >
-      <div className="bg-background flex flex-col">
-        <div className="relative aspect-square overflow-hidden">
-          <div>
-            {/* Primary Image */}
-            <div className="relative aspect-square flex items-center justify-center overflow-hidden">
-              {imageArray.length > 0 ? (
-                <Image
-                  src={`/${imageArray[0]}`}
-                  alt={product.name}
-                  //fill
-                  className="absolute inset-0 w-full h-full object-cover object-center transition-transform duration-500 ease-in-out"
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                />
-              ) : (
-                <div className="absolute inset-0 bg-muted flex items-center justify-center">
-                  <span className="text-muted-foreground">No image</span>
-                </div>
-              )}
-            </div>
-
-            {/* Secondary Image (if exists) */}
-            {imageArray.length > 1 && (
-              <div className="absolute inset-0 flex items-center justify-center">
-                <Image
-                  src={`/${imageArray[1]}`}
-                  alt={product.name}
-                  //fill
-                  className={`absolute inset-0 w-full h-full object-cover object-center transition-opacity duration-500 ease-in-out ${
-                    isHovering ? "opacity-100" : "opacity-0"
-                  }`}
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                />
-              </div>
-            )}
-          </div>
-
-          {/* Desktop Add to Cart button */}
-          <button
-            //onClick={handleAddToCart}
-            className={`absolute bottom-0 left-0 right-0 hidden md:flex items-center justify-center space-x-2 bg-muted/70 backdrop-blur-xs text-black hover:bg-black  transition-all duration-500 py-2 opacity-0 group-hover:opacity-100 ${
-              !isAvailable
-                ? "cursor-not-allowed hover:bg-muted/70 opacity-50"
-                : "hover:text-white"
-            }`}
-            disabled={!isAvailable}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="none"
-              viewBox="0 0 33 30"
-              className="cart-icon"
-            >
-              <path
-                d="M1.94531 1.80127H7.27113L11.9244 18.602C12.2844 19.9016 13.4671 20.8013 14.8156 20.8013H25.6376C26.9423 20.8013 28.0974 19.958 28.495 18.7154L31.9453 7.9303H19.0041"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <circle cx="13.4453" cy="27.3013" r="2.5" fill="currentColor" />
-              <circle cx="26.4453" cy="27.3013" r="2.5" fill="currentColor" />
-            </svg>
-            {!isAddingToCart ? (
-              <span>
-                {!isAvailable
-                  ? "Out of Stock"
-                  : isComingSoon
-                    ? "Pre-order"
-                    : "Add to Cart"}
-              </span>
-            ) : (
-              <span>{isComingSoon ? "Pre-ordering..." : "Adding..."}</span>
-            )}
-          </button>
-        </div>
-
-        {/* Content Section */}
-        <div className="flex flex-col h-auto md:h-full">
-          {/* Info Section */}
-          <div className="p-4 flex flex-col h-auto md:h-full">
-            {/* Price and Stock */}
-            <div className="flex flex-col mb-2">
-              <div className="flex flex-wrap items-baseline justify-between w-full gap-x-2">
-                <div className="flex flex-col items-baseline gap-1">
-                  {product.discount ? (
-                    <>
-                      <h5 className="whitespace-nowrap">
-                        $
-                        {/* {(currentPrice * (1 - product.discount / 100)).toFixed(
-                          2
-                        )}{" "} */}
-                        CAD
-                      </h5>
-                      <div className="flex items-center gap-1">
-                        <h6 className=" line-through text-muted-foreground">
-                          {/* ${currentPrice?.toFixed(2)} */}
-                        </h6>
-                        <Badge variant="green">{product.discount}% OFF</Badge>
-                      </div>
-                    </>
-                  ) : (
-                    <h5 className="whitespace-nowrap">
-                      {/* ${currentPrice?.toFixed(2)} CAD */}
-                    </h5>
-                  )}
-                </div>
-
-                {!product.unlimitedStock && (
-                  <div className="mt-1 text-xs">
-                    {/* {getEffectiveStock > 0 ? (
-                      <Badge variant="outline">
-                        <span className="text-muted-foreground">
-                          In stock:{" "}
-                        </span>
-                        <span>{getEffectiveStock}</span>
-                      </Badge>
-                    ) : (
-                      <Badge
-                        variant="outline"
-                        className="text-muted-foreground"
-                      >
-                        Out of stock
-                      </Badge>
-                    )} */}
+      <div
+        className="w-full product-card overflow-hidden rounded-lg group"
+        id={styles.productCard}
+      >
+        <div className="bg-background flex flex-col">
+          <div className="relative aspect-square overflow-hidden">
+            <div>
+              {/* Primary Image */}
+              <div className="relative aspect-square flex items-center justify-center overflow-hidden">
+                {imageArray.length > 0 ? (
+                  <Image
+                    src={`/${imageArray[0]}`}
+                    alt={product.name}
+                    //fill
+                    className="absolute inset-0 w-full h-full object-cover object-center transition-transform duration-500 ease-in-out"
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  />
+                ) : (
+                  <div className="absolute inset-0 bg-muted flex items-center justify-center">
+                    <span className="text-muted-foreground">No image</span>
                   </div>
                 )}
               </div>
 
-              {isComingSoon && (
-                <>
-                  <span className="text-sm hidden sm:inline">Coming Soon</span>
-                  <span className="text-sm w-full block sm:hidden mt-1">
-                    Coming Soon
-                  </span>
-                </>
+              {/* Secondary Image (if exists) */}
+              {imageArray.length > 1 && (
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <Image
+                    src={`/${imageArray[1]}`}
+                    alt={product.name}
+                    //fill
+                    className={`absolute inset-0 w-full h-full object-cover object-center transition-opacity duration-500 ease-in-out ${
+                      isHovering ? "opacity-100" : "opacity-0"
+                    }`}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  />
+                </div>
               )}
             </div>
 
-            {/* Product Name */}
-            <p className=" mb-3">{product.name}</p>
-
-            {/* Variations */}
-            {product.hasVariations &&
-              product.variations &&
-              product.variations.length > 0 && (
-                <div ref={variationsRef} className="space-y-2">
-                  {/* {attributeNames.map((attributeId: string) => (
-                    // <FilterGroup
-                    //   key={attributeId}
-                    //   title={getAttributeDisplayName(attributeId)}
-                    //   options={getUniqueAttributeValues(attributeId)}
-                    //   selectedOptions={selectedAttributes[attributeId] || null}
-                    //   onOptionChange={(value) => {
-                    //     if (value) {
-                    //       selectVariation(attributeId, value);
-                    //     }
-                    //   }}
-                    //   showAllOption={false}
-                    //   variant="product"
-                    //   getOptionAvailability={(value) =>
-                    //     isAttributeValueAvailable(attributeId, value)
-                    //   }
-                    // />
-                  ))} */}
-                </div>
-              )}
-          </div>
-
-          {/* Mobile Add to Cart button */}
-          <div className="md:hidden mt-auto">
+            {/* Desktop Add to Cart button */}
             <button
               //onClick={handleAddToCart}
-              className={`w-full flex items-center justify-center space-x-2 bg-muted backdrop-blur-xs text-black hover:bg-black  transition-all duration-500 py-2 px-4 ${
+              className={`absolute bottom-0 left-0 right-0 hidden md:flex items-center justify-center space-x-2 bg-muted/70 backdrop-blur-xs text-black hover:bg-black  transition-all duration-500 py-2 opacity-0 group-hover:opacity-100 ${
                 !isAvailable
-                  ? "opacity-50 cursor-not-allowed hover:bg-muted/70 hover:text-black"
+                  ? "cursor-not-allowed hover:bg-muted/70 opacity-50"
                   : "hover:text-white"
               }`}
               disabled={!isAvailable}
@@ -399,9 +264,157 @@ function ProductCard({ product }: { product: ProductWithVariations }) {
               )}
             </button>
           </div>
+
+          {/* Content Section */}
+          <div className="flex flex-col h-auto md:h-full">
+            {/* Info Section */}
+            <div className="p-4 flex flex-col h-auto md:h-full">
+              {/* Price and Stock */}
+              <div className="flex flex-col mb-2">
+                <div className="flex flex-wrap items-baseline justify-between w-full gap-x-2">
+                  <div className="flex flex-col items-baseline gap-1">
+                    {product.discount ? (
+                      <>
+                        <h5 className="whitespace-nowrap">
+                          $
+                          {/* {(currentPrice * (1 - product.discount / 100)).toFixed(
+                          2
+                        )}{" "} */}
+                          CAD
+                        </h5>
+                        <div className="flex items-center gap-1">
+                          <h6 className=" line-through text-muted-foreground">
+                            {/* ${currentPrice?.toFixed(2)} */}
+                          </h6>
+                          <Badge variant="green">{product.discount}% OFF</Badge>
+                        </div>
+                      </>
+                    ) : (
+                      <h5 className="whitespace-nowrap">
+                        {/* ${currentPrice?.toFixed(2)} CAD */}
+                      </h5>
+                    )}
+                  </div>
+
+                  {!product.unlimitedStock && (
+                    <div className="mt-1 text-xs">
+                      {/* {getEffectiveStock > 0 ? (
+                      <Badge variant="outline">
+                        <span className="text-muted-foreground">
+                          In stock:{" "}
+                        </span>
+                        <span>{getEffectiveStock}</span>
+                      </Badge>
+                    ) : (
+                      <Badge
+                        variant="outline"
+                        className="text-muted-foreground"
+                      >
+                        Out of stock
+                      </Badge>
+                    )} */}
+                    </div>
+                  )}
+                </div>
+
+                {isComingSoon && (
+                  <>
+                    <span className="text-sm hidden sm:inline">
+                      Coming Soon
+                    </span>
+                    <span className="text-sm w-full block sm:hidden mt-1">
+                      Coming Soon
+                    </span>
+                  </>
+                )}
+              </div>
+
+              {/* Product Name */}
+              <p className=" mb-3">{product.name}</p>
+
+              {/* Variations */}
+              {product.hasVariations &&
+                product.variations &&
+                product.variations.length > 0 && (
+                  <div ref={variationsRef} className="space-y-2">
+                    {/* {attributeNames.map((attributeId: string) => (
+                    // <FilterGroup
+                    //   key={attributeId}
+                    //   title={getAttributeDisplayName(attributeId)}
+                    //   options={getUniqueAttributeValues(attributeId)}
+                    //   selectedOptions={selectedAttributes[attributeId] || null}
+                    //   onOptionChange={(value) => {
+                    //     if (value) {
+                    //       selectVariation(attributeId, value);
+                    //     }
+                    //   }}
+                    //   showAllOption={false}
+                    //   variant="product"
+                    //   getOptionAvailability={(value) =>
+                    //     isAttributeValueAvailable(attributeId, value)
+                    //   }
+                    // />
+                  ))} */}
+                  </div>
+                )}
+            </div>
+
+            {/* Mobile Add to Cart button */}
+            <div className="md:hidden mt-auto">
+              <button
+                //onClick={handleAddToCart}
+                className={`w-full flex items-center justify-center space-x-2 bg-muted backdrop-blur-xs text-black hover:bg-black  transition-all duration-500 py-2 px-4 ${
+                  !isAvailable
+                    ? "opacity-50 cursor-not-allowed hover:bg-muted/70 hover:text-black"
+                    : "hover:text-white"
+                }`}
+                disabled={!isAvailable}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="none"
+                  viewBox="0 0 33 30"
+                  className="cart-icon"
+                >
+                  <path
+                    d="M1.94531 1.80127H7.27113L11.9244 18.602C12.2844 19.9016 13.4671 20.8013 14.8156 20.8013H25.6376C26.9423 20.8013 28.0974 19.958 28.495 18.7154L31.9453 7.9303H19.0041"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <circle
+                    cx="13.4453"
+                    cy="27.3013"
+                    r="2.5"
+                    fill="currentColor"
+                  />
+                  <circle
+                    cx="26.4453"
+                    cy="27.3013"
+                    r="2.5"
+                    fill="currentColor"
+                  />
+                </svg>
+                {!isAddingToCart ? (
+                  <span>
+                    {!isAvailable
+                      ? "Out of Stock"
+                      : isComingSoon
+                        ? "Pre-order"
+                        : "Add to Cart"}
+                  </span>
+                ) : (
+                  <span>{isComingSoon ? "Pre-ordering..." : "Adding..."}</span>
+                )}
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
