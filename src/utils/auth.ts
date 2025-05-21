d914b1cf08fbfba5cf3719e3ac4a1e10d978be57
alex-rublevsky <alexander.rublevskii@gmail.com>
2025-05-21 11:21:23 -0400

    misc

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,12 +2,12 @@ import { betterAuth } from "better-auth"
 import { reactStartCookies } from "better-auth/react-start";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import {db} from "~/db";
-import { schema } from "../schema";
+import { user, session, account, verification } from "../schema";
  
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "sqlite", 
-        schema: schema
+        schema: {user, session, account, verification}
     }),
     baseURL: process.env.BETTER_AUTH_URL,
     socialProviders: {
