794c4713f4e1be342b3e9918fbc9cc47d1a0945e
alex-rublevsky <alexander.rublevskii@gmail.com>
2025-05-23 16:47:36 -0400

    updated dashboard titles for pages

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,7 +1,18 @@
 import { Separator } from "~/components/ui/dashboard/separator";
 import { SidebarTrigger } from "~/components/ui/dashboard/sidebar";
+import { data } from "~/components/ui/dashboard/DashboardSidebar";
+import { useRouter, useRouterState } from "@tanstack/react-router";
 
 export function SiteHeader() {
+  const router = useRouter();
+  const routerState = useRouterState();
+  const currentPage =
+    router.state.location.pathname
+      .replace("/dashboard/", "")
+      .charAt(0)
+      .toUpperCase() +
+    router.state.location.pathname.replace("/dashboard/", "").slice(1);
+
   return (
     <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -10,7 +21,9 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium">Documents</h1>
+        <h1 className="!text-base !font-medium">
+          {currentPage ? currentPage : "Dashboard"}
+        </h1>
       </div>
     </header>
   );
