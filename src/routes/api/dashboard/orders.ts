37e890c524b3251c6a0a1ceef35e5508dae1d52b
alex-rublevsky <alexander.rublevskii@gmail.com>
2025-05-23 12:29:17 -0400

    added dashboard api's

---
+++ src/routes/api/dashboard/orders.ts
@@ -0,0 +1,42 @@
+import { json } from "@tanstack/react-start";
+import { createAPIFileRoute } from "@tanstack/react-start/api";
+import { orders } from "~/schema";
+import { db } from "~/db";
+
+export const APIRoute = createAPIFileRoute("/api/dashboard/products")({
+  GET: async ({ request, params }) => {
+    // Add CORS headers to allow requests from localhost
+    const corsHeaders = {
+      "Access-Control-Allow-Origin": "*",
+    };
+
+    try {
+      const productsResult = await db.select().from(orders).all();
+
+      if (!productsResult || productsResult.length === 0) {
+        return json(
+          { message: "No orders found" },
+          {
+            status: 404,
+            headers: corsHeaders,
+          }
+        );
+      }
+
+      const result = {
+        products: productsResult,
+      };
+
+      return json(result, { headers: corsHeaders });
+    } catch (error) {
+      console.error("Error fetching dashboard orders data:", error);
+      return json(
+        { error: "Failed to fetch dashboard orders data" },
+        {
+          status: 500,
+          headers: corsHeaders,
+        }
+      );
+    }
+  },
+});
