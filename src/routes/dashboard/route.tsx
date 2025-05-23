951a62d764d2635559877927d19d0b3581660f7a
alex-rublevsky <alexander.rublevskii@gmail.com>
2025-05-23 18:33:39 -0400

    updated dashboard route padding

--- src/routes/dashboard/route.tsx
+++ src/routes/dashboard/route.tsx
@@ -34,7 +34,7 @@ function RouteComponent() {
       <SidebarInset>
         <SiteHeader />
         <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
+          <div className="@container/main flex flex-1 flex-col gap-2 p-6 mb-8">
             <Outlet />
           </div>
         </div>
