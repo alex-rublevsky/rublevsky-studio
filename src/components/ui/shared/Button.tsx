1c4e79aec12b449aec5d7353265dc88587f91ae5
alex-rublevsky <alexander.rublevskii@gmail.com>
2025-05-31 17:56:38 -0400

    feat: enhance checkout form validation and button behavior

--- src/components/ui/shared/Button.tsx
+++ src/components/ui/shared/Button.tsx
@@ -6,7 +6,7 @@ import { useCursorContext } from "~/components/ui/shared/custom_cursor/CustomCur
 import { cn } from "~/utils/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all duration-300 ease-in-out focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 cursor-none",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all duration-300 ease-in-out focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:opacity-80 disabled:cursor-not-allowed [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 cursor-none",
   {
     variants: {
       variant: {
