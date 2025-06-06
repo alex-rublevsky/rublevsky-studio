f0323ec8f5000d3d5afff9931077d50f0508f750
alex-rublevsky <alexander.rublevskii@gmail.com>
2025-06-06 18:04:40 -0400

    chore: update baseURL in auth-client to use the new production domain

--- src/utils/auth-client.ts
+++ src/utils/auth-client.ts
@@ -1,6 +1,6 @@
 import {createAuthClient} from "better-auth/react";
 
-const baseURL = "https://tanstack.rublevsky.studio"
+const baseURL = "https://rublevsky.studio"
 //const baseURL = "http://localhost:3000";
 
 export const {useSession, signIn, signOut} = createAuthClient({
