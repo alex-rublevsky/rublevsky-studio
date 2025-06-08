67b263ddc2c26990bc4e0e7df11be5c0e244be00
alex-rublevsky <alexander.rublevskii@gmail.com>
2025-06-07 21:05:15 -0400

    feat: add TypeScript definitions for custom 'spline-viewer' JSX element

---
+++ src/types/spline.d.ts
@@ -0,0 +1,17 @@
+import React from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'spline-viewer': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        url?: string;
+        'loading-anim-type'?: string;
+      };
+    }
+  }
+}
+
+export {};
