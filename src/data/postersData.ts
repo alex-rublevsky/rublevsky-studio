cba8ee126318670ac4a75c593b3059bbcd0fc277
alex-rublevsky <alexander.rublevskii@gmail.com>
2025-06-20 14:13:28 +0700

    updated links and descriptions for my projects

--- src/data/postersData.ts
+++ src/data/postersData.ts
@@ -1,5 +1,6 @@
 import { GalleryItem } from '~/components/ui/studio/gallery/galleryTypes';
 
+//TODO: add logos for cinema 4d, photoshop, indesign
 export const posters: GalleryItem[] = [
   {
     id: 'artlab-poster',
@@ -9,15 +10,19 @@ export const posters: GalleryItem[] = [
   {
     id: 'capybara-cave',
     images: ['posters/capybara-cave.webp'],
-    description: 'Capybara in a cave'
+    description: 'A photo manipulation I have created using Photoshop'
   },
   {
     id: 'cyberpunk-capy',
-    images: ['posters/cyberpunk-capy.webp']
+    images: ['posters/cyberpunk-capy.webp'],
+    description: 'A photo manipulation I have created using Photoshop'
+
   },
   {
     id: 'fencing',
-    images: ['posters/fencing.webp']
+    images: ['posters/fencing.webp'],
+    description: 'A poster made with InDesign and Photoshop'
+
   },
   {
     id: 'graffiti-bark-abalych',
@@ -30,15 +35,19 @@ export const posters: GalleryItem[] = [
   },
   {
     id: 'ice-cold',
-    images: ['posters/ice-cold.webp']
+    images: ['posters/ice-cold.webp'],
+ description: 'Some Cinema4D experementation'
   },
   {
     id: 'madness',
-    images: ['posters/madness.webp']
+    images: ['posters/madness.webp'],
+    //TODO: spelling
+     description: 'Saturn devouvering his son. InDesign and Photoshop.'
   },
   {
     id: 'pelevin',
-    images: ['posters/pelevin.webp']
+    images: ['posters/pelevin.webp'],
+    description: `He's one of my favourite authors`
   },
   {
     id: 'quite',
@@ -50,11 +59,12 @@ export const posters: GalleryItem[] = [
   },
   {
     id: 'thin-and-fragile',
-    images: ['posters/thin-and-fragile.webp']
+    images: ['posters/thin-and-fragile.webp'],
+     description: `Some more Cinema4D experementation!`
   },
   {
     id: 'painting',
     images: ['posters/painting.webp'],
-    description: 'Painting'
+    description: 'I barely draw, but sometimes things come out'
   }
 ]; 
