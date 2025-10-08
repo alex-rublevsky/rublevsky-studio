b2c7ce20b89b0fa7a11bc801711e921deefc2ee5
alex-rublevsky <alexander.rublevskii@gmail.com>
2025-10-08 19:29:44 -0400

    misc

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig} from 'vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
-import { tanstackRouter } from '@tanstack/router-plugin/vite';
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { cloudflare } from '@cloudflare/vite-plugin';
-import { resolve } from 'node:path';
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import { resolve } from "node:path";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
@@ -17,14 +16,12 @@ export default defineConfig({
       projects: ["./tsconfig.json"],
     }),
     cloudflare({
-      viteEnvironment: { name: 'ssr' }
-      // wrangler: {
-      //   external: ['sqlite', 'blake3-wasm', 'better-sqlite3']
-      // }
+      viteEnvironment: { name: "ssr" },
+      configPath: "./wrangler.jsonc",
    }),
     tanstackStart(),
     tanstackRouter({
-      target: 'react',
+      target: "react",
       autoCodeSplitting: true,
     }),
     viteReact(),
@@ -32,7 +29,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
   },
   optimizeDeps: {
@@ -74,10 +71,10 @@ export default defineConfig({
     
   build: {
     rollupOptions: {
-      external: ['tsr:routes-manifest', 'sqlite', 'blake3-wasm'],
+      external: ["tsr:routes-manifest", "sqlite", "blake3-wasm"],
       // treeshake: {
       //   moduleSideEffects: 'no-external',
       // },
     },
-  }
+  },
 });
